69bacf1c3d3506eeb221edd55b9dabe6e7d8890f
GIERI <gieralt.artur@gmail.com>
2020-04-03 18:13:23 +0200

    Added aggregated query

---
+++ DataAccess/Models/AggregatedOffer.cs
@@ -0,0 +1,14 @@
+namespace DataAccess.Models
+{
+    public class AggregatedOffer
+    {
+        public double AverageArea { get; set; }
+        public string AreaUnit { get; set; }
+        public string PriceUnit {get; set; }
+        public string Type { get; set; }
+        public string City { get; set ;}
+        public double AveragePrice { get; set; }
+        public string Location { get; set; }
+        public string CreatedOn {get; set; }
+    }
+}
--- DataAccess/Repositories/OfferRepository.cs
+++ DataAccess/Repositories/OfferRepository.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DataAccess.Models;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace DataAccess.Repositories
@@ -18,6 +20,62 @@ namespace DataAccess.Repositories
             return await _context
                 .Offers
                 .FindSync(FilterDefinition<Offer>.Empty)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<AggregatedOffer>> GetAllAggregatedAsync()
+        {
+
+            var parseDate = new BsonDocument{ 
+                { "$project", new BsonDocument{ 
+                    {"DateAdded", new BsonDocument{
+                        {"$dateToString",new BsonDocument{
+                            { "format", "%Y-%m-%d"}, {"date", "$CreatedOn"} } 
+                        }}
+                    }}
+                }}; 
+
+            var groupByDate = new BsonDocument{ 
+                { "$group", new BsonDocument{ 
+                        { "_id", new BsonDocument{ 
+                            { "Type", "$Type"},
+                            { "City", "$City"},
+                            { "Location", "$Location"},
+                            { "CreatedOn", "$CreatedOn"}}
+                        },
+                        { "AverageTotalPrice", new BsonDocument{
+                            { "$avg", "$price"} } 
+                        },
+                        { "AverageArea", new BsonDocument{
+                            { "$avg", "$area"}} 
+                        },
+                        { "Count", new BsonDocument{
+                            { "$sum", 1}} 
+                        }}
+                }}; 
+
+            var chooseProperties = new BsonDocument{ 
+                { "$project", new BsonDocument{ 
+                    {"AverageTotalPrice", 1 },
+                    {"AverageArea", 1 },
+                    {"Count", 1 },
+                    {"CreatedOn", "$_id.CreatedOn" },
+                    {"City", "$_id.City" },
+                    {"Location", "$_id.Location" },
+                    {"Type", "$_id.Type" },
+                }}
+            };
+
+            PipelineDefinition<Offer, AggregatedOffer> pipeline = new BsonDocument[]{
+                parseDate,
+                groupByDate,
+                chooseProperties
+            };
+
+            return await _context
+                .Offers
+                .Aggregate(pipeline)
                 .ToListAsync()
                 .ConfigureAwait(false);
         }
